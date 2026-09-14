41ed3f5694d60fd4c4a5c0ae400649a336aa02e3
JHERSON <202500461@est.umss.edu>
2026-09-14 16:23:56 -0400

    DIbujar grafo:añadimos creacion y dibujo del grafo

--- interfaz.py
+++ interfaz.py
@@ -43,7 +43,63 @@ class Grafo:
 
 
     def crearGrafo(self):
-        return self.entrada.get()
+        n = int(self.entrada.get())
+        self.vertices = list(
+            range(1,n+1)
+        )
+        self.aristas = []
+        self.sel=None
+
+        self.dibujarGrafo()
+
+    def dibujarGrafo(self):
+        self.canvas.delete("all")
+
+        n = len(self.vertices)
+        if n == 0:
+            return
+        cx =350
+        cy =250
+        r = 180
+
+        for i, v in enumerate(self.vertices):
+
+            a = 2 * math.pi * i / n - math.pi / 2
+
+            self.pos[v] = (
+                cx + r * math.cos(a),
+                cy + r * math.sin(a)
+            )
+
+        for u, v in self.aristas:
+
+            self.canvas.create_line(
+                *self.pos[u],
+                *self.pos[v],
+                width=2
+            )
+
+        for v in self.vertices:
+
+            x, y = self.pos[v]
+
+            color = (
+                "yellow"
+                if v == self.sel
+                else "lightblue"
+            )        
+
+            self.canvas.create_oval(
+                x-20,
+                y-20,
+                x+20,
+                y+20,
+                fill=color
+            )
+            self.canvas.create_text(
+                x, y,
+                text=v
+            )
 
     def entregarGrafo(self):
         return self.vertices, self.aristas
